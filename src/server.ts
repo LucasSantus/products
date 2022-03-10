a249de0110d25ce55fd7e11d44a602a94039b4ff
Lucas Santos <leos9877@gmail.com>
2022-03-10 18:27:31 -0300

    :pushpin: inserindo alterações no projeto

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import swaggerDocs from "./swagger.json"
 
 const app = express();
 
+const PORT = 3000
+
 app.use(express.json());
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
@@ -18,4 +20,7 @@ app.get("/terms", (request, response) => {
 })
 
 app.use("/v1", router);
-app.listen(3000, () => console.log("Server is running on port 3000"));
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
